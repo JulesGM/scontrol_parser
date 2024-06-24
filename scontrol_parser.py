318f4d6752a86556763dda9e6a511d67ff7dc394
Jules Gagnon-Marchand <jules.gagnonm.alt@gmail.com>
2024-06-24 01:16:46 -0400

    removed sorting

--- scontrol_parser.py
+++ scontrol_parser.py
@@ -3,18 +3,14 @@ import json
 import subprocess as sp
 
 
-def call(job_id=None, sort=False):
+def call(job_id=None):
   if job_id is None:
     assert "SLURM_JOB_ID" in os.environ, "Either need a value for the argument job_id or a value for the argument $SLURM_JOB_ID"
     job_id = os.environ["SLURM_JOB_ID"]
 
   output = sp.check_output(["scontrol", "--json", "show", "job", str(job_id)], text=True).strip()
-  parsed = json.loads(output)
-  assert len(parsed) == 1, parsed
-  parsed = parsed["jobs"]
+  parsed = json.loads(output)["jobs"]
   assert len(parsed) == 1, parsed
   parsed = parsed[0]
-  if sort:
-    parsed = dict(sorted(parsed.items()))
 
   return parsed
